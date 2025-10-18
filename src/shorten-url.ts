7caa1e3a298a8e05b01cda3380869f99231f8d8c
idiotf <aqu3180@naver.com>
2025-10-18 17:10:12 +0900

    url 검증 관련 수정

--- src/shorten-url.ts
+++ src/shorten-url.ts
@@ -5,6 +5,7 @@ function urlWithParams(baseUrl: string | URL, params: Record<string, string>) {
 }
 
 export default async function getShortenUrl(url: string) {
+  url = url.replaceAll('script', 'scrip%74')
   const res = await fetch(urlWithParams('https://me2do.naver.com/common/requestJsonpV2.nhn', {
     svcCode: '0',
     url: urlWithParams('https://link.naver.com/bridge', {
@@ -14,7 +15,7 @@ export default async function getShortenUrl(url: string) {
         version: '10',
         sourceReferer: 'share',
       }),
-    }).replaceAll('script', 'scrip%74'),
+    }),
   }))
   if (!res.ok) throw `error code ${res.status}`
 
@@ -22,6 +23,6 @@ export default async function getShortenUrl(url: string) {
   const body = JSON.parse(text.slice(1, -2))
   if (body.code != '200') throw `${body.code} ${body.message}`
 
-  const shortenUrl = body.result.url as string
+  const shortenUrl: string = body.result.url
   return shortenUrl.replace('https://', '')
 }
